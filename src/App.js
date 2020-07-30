7780f9adf19d05c25f0fbfd433a4a4b8c055ab5f
akbariyansyah <riyansean21366@gmail.com>
2020-07-30 10:43:06 +0700

    added new componenet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Weather/>
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Weather() {
+    return (
+        <div>
+            <h1>This is from weather</h1>
+        </div>
+    )
+}
