45463b5c577e62e42a96df974dbc4edb44c418a9
akbariyansyah <riyansean21366@gmail.com>
2020-08-01 07:53:45 +0700

    --added enzyme -added few unit testing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import MainRouter from './components/router/MainRouter';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap.js';
 function App() {
   return (
     <div className="container">
       <div className="row">
-
         <div className="col-lg-12">
             <MainRouter />
         </div>
       </div>
-
-
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme'
+import Card from './components/common/Card'
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />)
+  if (state) wrapper.setState(state)
+  return wrapper
+}
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App/>', () => {
+  it('should be in <App/> component to be in three div', () => {
+    // shallow => mengambil isi dari component
+    const appComponent = setup()
+    // hasil yang diharapkan 
+    expect(appComponent.find('div').length).toBe(3)
+  })
+})
+describe('<Card/>', () => {
+  it('should be in <Card/> component to be in called once', () => {
+    // shallow => mengambil isi dari component
+    const cardComponent = shallow(<Card />)
+    // hasil yang diharapkan 
+    expect(cardComponent.find("table").length).toEqual(1)
+  })
+
+})
+
